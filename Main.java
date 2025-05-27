c7fb2cd8863fe62f33fd83e1cdb97324b118623d
smritiramann <smritiramann@gamil.com>
2025-05-27 16:22:52 +0530

    third commit

--- Main.java
+++ Main.java
@@ -2,6 +2,7 @@ public class Main{
     public static void Main(string[] args)
    {
     system.out.println("hello world");
-    system.out.println("hello world again")
+    system.out.println("hello world again");
+    system.out.println("jai mata di");
    
 }
